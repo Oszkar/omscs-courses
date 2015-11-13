1afc8e47a5bd4f04ffde6ad66caeb1e18ecb0119
Oszkar <jozsa.oszkar@gmail.com>
2015-11-13 16:28:08 +0100

    Code cleanup

--- specialization.ts
+++ specialization.ts
@@ -1,13 +1,26 @@
 ﻿module App {
 
+    /**
+     * Holds a group of courses.
+     * @class
+     */
     export class courseGroup {
+        /** @property {string} groupName The name/title of this group */
         groupName: string;
+        /** @property {number} minCourses Minimum number ouf courses that have to be completed from this group */
         minCourses: number;
+        /** @property {number[]} courseList Array of course ids that are within this group */
         courseList: number[];
     }
 
+    /**
+     * Holds groups of courses. Serves as a master group for core and elective courses that can hold additional sub-groups
+     * @class
+     */
     export class masterGroup {
+        /** @property {number} minCourses Minimum number ouf courses that have to be completed */
         minCourses: number;
+        /** @property {courseGroup} groups The course groups */
         groups: courseGroup[];
     }
 
@@ -18,9 +31,9 @@
     export class Specialization {
         /** @property {string} title Title or name of the specialization */
         title: string;
-        /** @property */
+        /** @property {masterGroup} core The core courses of this specialization */
         core: masterGroup;
-        /** @property */
+        /** @property {masterGroup} electives The elective courses of this specialization */
         electives: masterGroup;
         /** @property {string } url GaTech website url of the specialization */
         url: string;
