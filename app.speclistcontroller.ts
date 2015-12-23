6dd06b3cd772e9b7b460317488e0d74ae255fbc3
Oszkar <jozsa.oszkar@gmail.com>
2015-12-23 01:26:02 +0100

    Add speclistcontroller (empty for now)

---
+++ app.speclistcontroller.ts
@@ -0,0 +1,15 @@
+﻿module App {
+    export class SpecListController extends App.CourseMatrixController {
+        constructor($scope: ng.IScope, NgTableParams) {
+            super($scope, NgTableParams);
+            this.tableParams = new NgTableParams(
+                {
+                    count: 70 // initial page size
+                },
+                {
+                    counts: [],
+                    dataset: this._specializations
+                });
+        }
+    }
+}
